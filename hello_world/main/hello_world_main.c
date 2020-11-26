0c3fa3d8de2821263352d18c7eb27349a97cd15e
Kevin Seegobin <kevin.seegobin@my.uwi.edu>
2020-11-26 09:24:38 -0400

    Change hello notification and time before reset

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -15,7 +15,7 @@
 
 void app_main()
 {
-    printf("Hello world!\n");
+    printf("Hello Kevin Seegobin\n You're ID number is: 816014799");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -28,10 +28,10 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
+    for (int i = (99 % 15); i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+    }   
     printf("Restarting now.\n");
     fflush(stdout);
     esp_restart();
